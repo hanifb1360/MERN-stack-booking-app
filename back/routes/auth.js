167e33669f03993933427a6f314de63c420b824c
hanifb1360 <hanif.bahari@gmail.com>
2022-05-11 23:33:27 +0200

    first commit-initial backend structure and empty react front

---
+++ back/routes/auth.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+
+router.get("/", (req, res)=>{
+    res.send("this is auth endpoint!")
+});
+router.get("/register", (req, res)=>{
+    res.send("this is register endpoint!")
+});
+
+export default router
